3de4624efa91e07d566ff3dcfd68dd177ec6a79d
Moshiur Rahman <merajlovesall@gmail.com>
2014-09-30 13:42:16 +0600

    Trip model is added
    Some properties are added in Trip model

--- VMS/VMS/Models/Trip.cs
+++ VMS/VMS/Models/Trip.cs
@@ -8,5 +8,11 @@ namespace VMS.Models
     public class Trip
     {
         public int TripId { get; set; }
+        public string CarAssignedPerson { get; set; }
+        public string CarAssignedPersonDesignation { get; set; }
+        public DateTime TripStartDateTime { get; set; }
+        public DateTime TripEndDateTime { get; set; }
+        public string Destination { get; set; }
+        
     }
 }
